85df47b374ca60e6c312841de8ea406141e440dc
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-08-29 08:47:25 +0200

    strings-exercises

---
+++ strings.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("\u{1f385}");
+
+const longline =
+  "A very, very, very, very, very, very, very lone line of text, \nthat spans across the width of the editor";
+
+const name = "Peter";
+const drink = `Coca-cola`;
+
+const text = `Hello ${name}, 
+would you like a ${drink}?`;
+
+console.log(text);
---
+++ template.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const name = "Peter";
+const animalType = "cat";
+const animalName = "Mandu";
+
+console.log(`My name is ${name}.
+I have a ${animalType} called ${animalName}`);
